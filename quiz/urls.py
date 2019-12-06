dce64afa2cbc1e3cb987d9f5fbf763b90d95d820
Nursultan <abdnurs2001@gmail.com>
2019-12-06 22:35:22 +0600

    second project

---
+++ quiz/urls.py
@@ -0,0 +1,52 @@
+from django.conf.urls import url
+from . import views
+from django.urls import path
+from .views import QuizListView, CategoriesListView,\
+    ViewQuizListByCategory, QuizUserProgressView, QuizMarkingList,\
+    QuizMarkingDetail, QuizDetailView, QuizTake, index, login_user, logout_user
+from .views import  SearchResultsView
+
+
+urlpatterns = [    
+         url(regex=r'^$', view=index, name='index'),
+                        url(regex=r'^login/$', view=login_user, name='login'),
+                        url(regex=r'^logout/$', view=logout_user, name='logout'),
+                       url(regex=r'^quizzes/$',
+                           view=QuizListView.as_view(),
+                           name='quiz_index'),
+                       
+
+
+                       url(regex=r'^category/$',
+                           view=CategoriesListView.as_view(),
+                           name='quiz_category_list_all'),
+
+                       url(regex=r'^category/(?P<category_name>[\w|\W-]+)/$',
+                           view=ViewQuizListByCategory.as_view(),
+                           name='quiz_category_list_matching'),
+
+                       url(regex=r'^progress/$',
+                           view=QuizUserProgressView.as_view(),
+                           name='quiz_progress'),
+
+                       url(regex=r'^marking/$',
+                           view=QuizMarkingList.as_view(),
+                           name='quiz_marking'),
+
+                       url(regex=r'^marking/(?P<pk>[\d.]+)/$',
+                           view=QuizMarkingDetail.as_view(),
+                           name='quiz_marking_detail'),
+
+                       #  passes variable 'quiz_name' to quiz_take view
+                       url(regex=r'^(?P<slug>[\w-]+)/$',
+                           view=QuizDetailView.as_view(),
+                           name='quiz_start_page'),
+
+                       url(regex=r'^(?P<quiz_name>[\w-]+)/take/$',
+                           view=QuizTake.as_view(),
+                           name='quiz_question'),
+path("tutor/", views.tutor, name="tutor"),
+path("student/", views.student, name="student"),
+path('', HomePageView.as_view(), name='home'),
+
+]
